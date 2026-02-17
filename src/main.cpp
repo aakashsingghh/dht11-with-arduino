9ab695f65a984560278488870007535696eba2ad
aakashsingghh <akashsingh33280@gmail.com>
2026-02-17 09:13:17 +0530

    initialize DHT sensor and update main loop for temperature and humidity readings

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("Initalizing DHT sensor!");
+
 }
 
 void loop() {
 // write your code here
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+    Serial.println("Humidity: ");
+    Serial.println(humidity);
+    Serial.println("Temperature: ");
+    Serial.println(temperature);
+    delay(2000);
 }
